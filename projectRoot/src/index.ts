b6e50e81c152255590d02460ffc68a3f6e229673
weidian <youzhijun@weidian.com>
2019-12-16 10:31:57 +0800

    从js迁移到ts

---
+++ projectRoot/src/index.ts
@@ -0,0 +1,15 @@
+class Point {
+	constructor(public x, public y) {}
+	getDistance(p: Point) {
+		let dx = p.x - this.x;
+		let dy = p.y - this.y;
+		return
+	}
+}
+
+interface Point {
+	distanceFromOrigin(point: Point): number;
+}
+Point.prototype.distanceFromOrigin = function(point: Point) {
+	return this.getDistance({x: 0, y: 0});
+}
